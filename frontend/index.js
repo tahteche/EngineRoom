842a1656f626d78226262666416e4c4ed3e56ff2
Teche Tah <tahteche@gmail.com>
2020-07-05 14:03:48 -0500

    Clean up

--- frontend/index.js
+++ frontend/index.js
@@ -1,58 +1,28 @@
 import React, {Fragment, useState, useCallback, useEffect} from 'react';
-import {cursor} from '@airtable/blocks';
+import {cursor, settingsButton} from '@airtable/blocks';
 import {ViewType} from '@airtable/blocks/models';
 import {
     initializeBlock,
-    registerRecordActionDataCallback,
     useBase,
     useRecordById,
     useLoadable,
-    useSettingsButton,
     useWatchable,
     Box,
     Dialog,
     Heading,
-    Link,
     Text,
     TextButton,
 } from '@airtable/blocks/ui';
 
-import {useSettings} from './settings';
-import SettingsForm from './SettingsForm';
 import EngineRoomCore from './EngineRoomCore.js';
 
 // How this block chooses a preview to show:
 //
-// Without a specified Table & Field:
-//
 //  - When the user selects a cell in grid view and the field's content is
-//    a supported preview URL, the block uses this URL to construct an embed
-//    URL and inserts this URL into an iframe.
-//
-// To Specify a Table & Field:
-//
-//  - The user may use "Settings" to toggle a specified table and specified
-//    field constraint. If the constraint switch is set to "Yes",he user must
-//    set a specified table and specified field for URL previews.
-//
-// With a specified table & specified field:
-//
-//  - When the user selects a cell in grid view and the active table matches
-//    the specified table or when the user opens a record from a button field
-//    in the specified table:
-//    The block looks in the selected record for the
-//    specified field containing a supported URL (e.g. https://www.youtube.com/watch?v=KYz2wyBy3kc),
-//    and uses this URL to construct an embed URL and inserts this URL into
-//    an iframe.
-//
+//    a URL or attachment of a supported file type, the block displays a preview.
 function EngineRoom() {
-    const [isSettingsOpen, setIsSettingsOpen] = useState(false);
-    useSettingsButton(() => setIsSettingsOpen(!isSettingsOpen));
-
-    const {
-        isValid,
-        settings: {isEnforced, urlTable},
-    } = useSettings();
+    // There are no settings so hide the settings button
+    settingsButton.hide()
 
     // Caches the currently selected record and field in state. If the user
     // selects a record and a preview appears, and then the user de-selects the
@@ -61,7 +31,6 @@ function EngineRoom() {
     const [selectedRecordId, setSelectedRecordId] = useState(null);
     const [selectedFieldId, setSelectedFieldId] = useState(null);
 
-    const [recordActionErrorMessage, setRecordActionErrorMessage] = useState('');
 
     // cursor.selectedRecordIds and selectedFieldIds aren't loaded by default,
     // so we need to load them explicitly with the useLoadable hook. The rest of
@@ -86,47 +55,6 @@ function EngineRoom() {
         }
     });
 
-    // Close the record action error dialog whenever settings are opened or the selected record
-    // is updated. (This means you don't have to close the modal to see the settings, or when
-    // you've opened a different record.)
-    useEffect(() => {
-        setRecordActionErrorMessage('');
-    }, [isSettingsOpen, selectedRecordId]);
-
-    // Register a callback to be called whenever a record action occurs (via button field)
-    // useCallback is used to memoize the callback, to avoid having to register/unregister
-    // it unnecessarily.
-    const onRecordAction = useCallback(
-        data => {
-            // Ignore the event if settings are already open.
-            // This means we can assume settings are valid (since we force settings to be open if
-            // they are invalid).
-            if (!isSettingsOpen) {
-                if (isEnforced) {
-                    if (data.tableId === urlTable.id) {
-                        setSelectedRecordId(data.recordId);
-                    } else {
-                        // Record is from a mismatching table.
-                        setRecordActionErrorMessage(
-                            `This block is set up to preview URLs using records from the "${urlTable.name}" table, but was opened from a different table.`,
-                        );
-                    }
-                } else {
-                    // Preview is not supported in this case, as we wouldn't know what field to preview.
-                    // Show a dialog to the user instead.
-                    setRecordActionErrorMessage(
-                        'You must enable "Use a specific field for previews" to preview URLs with a button field.',
-                    );
-                }
-            }
-        },
-        [isSettingsOpen, isEnforced, urlTable],
-    );
-    useEffect(() => {
-        // Return the unsubscribe function to ensure we clean up the handler.
-        return registerRecordActionDataCallback(onRecordAction);
-    }, [onRecordAction]);
-
     // This watch deletes the cached selectedRecordId and selectedFieldId when
     // the user moves to a new table or view. This prevents the following
     // scenario: User selects a record that contains a preview url. The preview appears.
@@ -141,13 +69,6 @@ function EngineRoom() {
     const base = useBase();
     const activeTable = base.getTableByIdIfExists(cursor.activeTableId);
 
-    useEffect(() => {
-        // Display the settings form if the settings aren't valid.
-        if (!isValid && !isSettingsOpen) {
-            setIsSettingsOpen(true);
-        }
-    }, [isValid, isSettingsOpen]);
-
     // activeTable is briefly null when switching to a newly created table.
     if (!activeTable) {
         return null;
@@ -155,25 +76,11 @@ function EngineRoom() {
 
     return (
         <Box>
-            {isSettingsOpen ? (
-                <SettingsForm setIsSettingsOpen={setIsSettingsOpen} />
-            ) : (
-                <RecordPreviewWithDialog
-                    activeTable={activeTable}
-                    selectedRecordId={selectedRecordId}
-                    selectedFieldId={selectedFieldId}
-                    setIsSettingsOpen={setIsSettingsOpen}
-                />
-            )}
-            {recordActionErrorMessage && (
-                <Dialog onClose={() => setRecordActionErrorMessage('')} maxWidth={400}>
-                    <Dialog.CloseButton />
-                    <Heading size="small">Can&apos;t preview URL</Heading>
-                    <Text variant="paragraph" marginBottom={0}>
-                        {recordActionErrorMessage}
-                    </Text>
-                </Dialog>
-            )}
+            <RecordPreviewWithDialog
+                activeTable={activeTable}
+                selectedRecordId={selectedRecordId}
+                selectedFieldId={selectedFieldId}
+            />
         </Box>
     );
 }
@@ -184,7 +91,6 @@ function RecordPreviewWithDialog({
     activeTable,
     selectedRecordId,
     selectedFieldId,
-    setIsSettingsOpen,
 }) {
     const [isDialogOpen, setIsDialogOpen] = useState(false);
 
@@ -212,30 +118,16 @@ function RecordPreviewWithDialog({
                     selectedRecordId={selectedRecordId}
                     selectedFieldId={selectedFieldId}
                     setIsDialogOpen={setIsDialogOpen}
-                    setIsSettingsOpen={setIsSettingsOpen}
                 />
             </Box>
             {isDialogOpen && (
                 <Dialog onClose={() => setIsDialogOpen(false)} maxWidth={400}>
                     <Dialog.CloseButton />
-                    <Heading size="small">Supported services</Heading>
-                    <Text marginTop={2}>Previews are supported for these services:</Text>
+                    <Heading size="small">Supported File Types</Heading>
+                    <Text marginTop={2}>Previews are supported for the following files types:</Text>
                     <Text marginTop={2}>
-                        <Link
-                            href="https://support.airtable.com/hc/en-us/articles/205752117-Creating-a-base-share-link-or-a-view-share-link"
-                            target="_blank"
-                        >
-                            Airtable share links
-                        </Link>
-                        , Figma, SoundCloud, Spotify, Vimeo, YouTube
+                        GLB, GLTF, and OBJ
                     </Text>
-                    <Link
-                        marginTop={2}
-                        href="https://airtable.com/shrQSwIety6rqfJZX"
-                        target="_blank"
-                    >
-                        Request a new service
-                    </Link>
                 </Dialog>
             )}
         </Fragment>
@@ -248,19 +140,15 @@ function RecordPreview({
     selectedRecordId,
     selectedFieldId,
     setIsDialogOpen,
-    setIsSettingsOpen,
 }) {
-    const {
-        settings: {isEnforced, urlField, urlTable},
-    } = useSettings();
 
-    const table = (isEnforced && urlTable) || activeTable;
+    const table = activeTable;
 
     // We use getFieldByIdIfExists because the field might be deleted.
     const selectedField = selectedFieldId ? table.getFieldByIdIfExists(selectedFieldId) : null;
     // When using a specific field for previews is enabled and that field exists,
     // use the selectedField
-    const previewField = (isEnforced && urlField) || selectedField;
+    const previewField = selectedField;
     // Triggers a re-render if the record changes. Preview URL cell value
     // might have changed, or record might have been deleted.
     const selectedRecord = useRecordById(table, selectedRecordId ? selectedRecordId : '', {
@@ -274,28 +162,11 @@ function RecordPreview({
     // This button is re-used in two states so it's pulled out in a constant here.
     const viewSupportedURLsButton = (
         <TextButton size="small" marginTop={3} onClick={() => setIsDialogOpen(true)}>
-            View supported URLs
+            View supported files.
         </TextButton>
     );
 
     if (
-        // If there is/was a specified table enforced, but the cursor
-        // is not presently in the specified table, display a message to the user.
-        // Exception: selected record is from the specified table (has been opened
-        // via button field or other means while cursor is on a different table.)
-        isEnforced &&
-        cursor.activeTableId !== table.id &&
-        !(selectedRecord && selectedRecord.parentTable.id === table.id)
-    ) {
-        return (
-            <Fragment>
-                <Text paddingX={3}>Switch to the “{table.name}” table to see previews.</Text>
-                <TextButton size="small" marginTop={3} onClick={() => setIsSettingsOpen(true)}>
-                    Settings
-                </TextButton>
-            </Fragment>
-        );
-    } else if (
         // activeViewId is briefly null when switching views
         selectedRecord === null &&
         (cursor.activeViewId === null ||
@@ -316,9 +187,8 @@ function RecordPreview({
             </Fragment>
         );
     } else {
-        // Using getCellValueAsString guarantees we get a string back. If
-        // we use getCellValue, we might get back numbers, booleans, or
-        // arrays depending on the field type.
+        // Using getCellValue because previews of URLs and Airtable Attachments
+        // are supported
         const cellValue = selectedRecord.getCellValue(previewField);
         if (!cellValue) {
             return (
