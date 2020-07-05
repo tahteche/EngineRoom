53d730c64c83e9c63084fb35fb142ec377e45cc7
Teche Tah <tahteche@gmail.com>
2020-07-05 17:17:31 -0500

    Documentation

--- frontend/EngineRoomCore.js/index.js
+++ frontend/EngineRoomCore.js/index.js
@@ -8,13 +8,15 @@ import {viewport} from '@airtable/blocks';
 const EngineRoomCore = ({src}) => {
     const [canvas, setCanvas] = useState()
 
+    // Need to resize the engine when ever the canvas resizes e.g when this block
+    // or the blocks sidebar goes into fullscreen mode
     const onResize = () => {
         if (engine) {
             engine.resize()
         }
     }
 
-
+    // Babylon Engine depends on the existence of a canvas so create it when the canvas has been setup
     const [engine, setEngine] = useState()
     useEffect(() => {
         if (canvas) {
@@ -22,6 +24,7 @@ const EngineRoomCore = ({src}) => {
         }
     }, [canvas])
 
+    // Babylon Scene depends on the existence of Babylon Engine so create it when the Engine has been setup
     const [scene, setScene] = useState()
     useEffect(() => {
         if (engine) {
@@ -30,6 +33,8 @@ const EngineRoomCore = ({src}) => {
         }
     }, [engine])
 
+    // After Babylon Scene has been setup then configure camera, import the 3D mesh (the asset to be previewed)
+    // and setup the environment so its well centered, etc
     useEffect(() => {
         if (scene) {
 
@@ -52,6 +57,8 @@ const EngineRoomCore = ({src}) => {
         }
     }, [scene])
 
+    // When ever the asset to be previewd changes clear the environment and set things up again to preview
+    // the new asset. We don't display both assets
     useEffect(() => {
         if (scene){
             engine.dispose()
@@ -62,11 +69,15 @@ const EngineRoomCore = ({src}) => {
     const setCanvasAndObserveSize = (canvas) => {
         if (canvas){
             setCanvas(canvas)
+
+            // When the blocks sidebar (not the block itself) is in fullscreen mode it modifies the size of the block
+            // this will catch that and resize the engine, keeping things need
             let resizeObserver = new ResizeObserver(onResize)
             resizeObserver.observe(canvas)
         }
     }
 
+    // Resize engine when the block is in fullscreen mode
     viewport.watch("isFullscreen", onResize)
 
     return <canvas ref={(ref) => {setCanvasAndObserveSize(ref)}} style={{width: "100%", height:"100%"}} ></canvas>
