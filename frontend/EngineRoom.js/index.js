7e9620a3ddb49241bf68acdd46451c06fc1af57a
Teche Tah <tahteche@gmail.com>
2020-07-05 12:03:13 -0500

    Fix issue when switching directly between cells containing 3D previews Property rendering in fullscreen mode

--- frontend/EngineRoom.js/index.js
+++ frontend/EngineRoom.js/index.js
@@ -1,17 +1,16 @@
 import React, {useState, useEffect} from 'react'
 import {Engine, Scene, ArcRotateCamera, SceneLoader} from 'babylonjs'
 import 'babylonjs-loaders'
+import {viewport} from '@airtable/blocks';
 
 
 
 const EngineRoom = ({src}) => {
     const [canvas, setCanvas] = useState()
-
     const [engine, setEngine] = useState()
     useEffect(() => {
         if (canvas) {
-            let e = new Engine(canvas)
-            setEngine(e)
+            setEngine(createEngine(canvas))
             console.log("engine set")
             console.log(canvas, engine)
         }
@@ -20,8 +19,8 @@ const EngineRoom = ({src}) => {
     const [scene, setScene] = useState()
     useEffect(() => {
         if (engine) {
-            let s = new Scene(engine)
-            setScene(s)
+            canvas.addEventListener("resize", onResize)
+            setScene(createScene(engine))
             console.log("scene is set")
             console.log(engine)
         }
@@ -30,20 +29,20 @@ const EngineRoom = ({src}) => {
     useEffect(() => {
         if (scene) {
 
+            let camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 0, BABYLON.Vector3.Zero(), scene);
+            camera.setPosition(new BABYLON.Vector3(5, 5, -5));
+            camera.attachControl(canvas, true);
+
+            console.log("loader is set")
+            console.log(scene)
+
             SceneLoader.ImportMesh("", src, undefined, scene,
                 (meshes) => {
                     console.log("yolo", meshes)
                     scene.createDefaultCameraOrLight(true, true, true);
                     scene.createDefaultEnvironment();
-            })
-
-            let camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 0, BABYLON.Vector3.Zero(), scene);
-            camera.setPosition(new BABYLON.Vector3(5, 5, -5));
-            camera.attachControl(canvas, true);
-            let light = new BABYLON.HemisphericLight("hemi", new BABYLON.Vector3(0, 1, 0), scene);
-
-            console.log("loader is set")
-            console.log(scene)
+                }
+            )
 
             engine.runRenderLoop(() => {
                 scene.render()
@@ -53,7 +52,37 @@ const EngineRoom = ({src}) => {
         }
     }, [scene])
 
-    return <canvas ref={(ref) => {setCanvas(ref)}}></canvas>
+    useEffect(() => {
+        if (scene){
+            engine.dispose()
+            setEngine(createEngine(canvas))
+        }
+    }, [src])
+
+    const setCanvasAndSize = (canvas) => {
+        if (canvas){
+            setCanvas(canvas)
+        }
+    }
+
+    const onResize = () => {
+        if (engine) {
+            engine.resize()
+        }
+    }
+    viewport.watch("isFullscreen", onResize)
+
+
+    return <canvas ref={(ref) => {setCanvasAndSize(ref)}} style={{width: "100%", height:"100%"}} ></canvas>
+}
+
+const createScene = (engine) => {
+    let s = new Scene(engine)
+    return s
+}
+
+const createEngine = (canvas) => {
+     return new Engine(canvas)
 }
 
 export default EngineRoom
